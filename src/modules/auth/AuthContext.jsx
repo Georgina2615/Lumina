889c0276197fe06b2e0ffb9d3a4cfb7d01ed5dcf
Georgina <reyesjoely448@gmail.com>
2026-07-20 17:05:53 -0600

    refactor: organización de estructuro

--- src/modules/auth/AuthContext.jsx
+++ src/modules/auth/AuthContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { app } from "../config/firebase";
-import { iniciarSesionGoogle, cerrarSesionApp, obtenerRolUsuario } from "../services/authService";
+import { app } from "../../config/firebase";
+import { iniciarSesionGoogle, cerrarSesionApp, obtenerRolUsuario } from "../../services/authService";
 
 const AuthContext = createContext();
 
